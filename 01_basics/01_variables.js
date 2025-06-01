73baa1cbd091a5b9b38375f79f2b6e04708d416a
mdsamura <samuramohammad@gmail.com>
2025-06-01 15:10:44 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountID = 144553
+let accountEmail = "samura@google.com"
+var accountPassword = "12345"
+accountCity = "Dhaka"
+
+
+//accountID = 2// not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "faridpur"
+
+
+console.log(accountID);
+
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table({accountID,accountEmail,accountPassword,accountCity})
